17c79c09cdb8d42e71d9b6b221116fc32fd559e4
José Carlos <30965492+jcval94@users.noreply.github.com>
2019-07-22 08:59:45 +0100

    App

---
+++ App.R
@@ -0,0 +1,32 @@
+source(paste0(getwd(),"/Read_docs.R"))
+library(shiny)
+server<-function(input, output) {
+  
+  observeEvent(c(input$txt,input$Dir,input$Deep),{
+    DT<-try(Read_docs(input$Dir,text_ = input$txt,deep = input$Deep),silent = T)
+    if(is.error(DT)){DT<-data.frame(Message="File not found")}
+    print(input$Dir)
+    print(input$txt)
+    output$table <- DT::renderDataTable({
+      DT
+    },
+    options = list(scrollX = TRUE))
+  })
+  
+  
+  
+}
+
+ui<-fluidPage(
+    titlePanel("Basic Finder"),
+    fluidRow(
+      column(4,
+             textInput("Dir","File address",value = getwd()),
+             textInput("txt","Text to find",value = "Hello World!"),
+             checkboxInput(inputId = "Deep","Search in subfolders?",value = FALSE)
+      )
+    ),
+    DT::dataTableOutput("table")
+  )
+
+shinyApp(ui,server)
